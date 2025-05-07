478a893694649f82d780fbae26836f85268b88d6
Felipe Cortez <hola@faco.cl>
2025-05-06 21:47:49 -0400

    Update semestres.js

--- clase-08/js/semestres.js
+++ clase-08/js/semestres.js
@@ -45,7 +45,7 @@ async function datos(criterio) {
     console.log("Lo que sigue son todos los datos:");
     console.log(data);
 
-    data.forEach((d) => {
+    data.data.forEach((d) => {
         if (d.semestre_examen == criterio) {
             seleccion.push(d);
         }
